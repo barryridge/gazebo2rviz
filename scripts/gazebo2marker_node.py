81bab4a25615160c96c71ceb2b283ec56250e126
Barry Ridge <bridge@jpl.nasa.gov>
2021-03-22 23:48:08 -0700

    Add args for max num. of marker messages, latching & message lifetime

--- scripts/gazebo2marker_node.py
+++ scripts/gazebo2marker_node.py
@@ -21,14 +21,19 @@ world = None
 model_cache = {}
 lastUpdateTime = None
 worldsdf = None
-
+latch = False
+max_messages = -1
+message_count = 0
+lifetime = 1
 
 
 def publish_link_marker(link, full_linkname, **kwargs):
   full_linkinstancename = full_linkname
   if 'model_name' in kwargs and 'instance_name' in kwargs:
     full_linkinstancename = full_linkinstancename.replace(kwargs['model_name'], kwargs['instance_name'], 1)
-  marker_msgs = link2marker_msg(link, full_linkinstancename, use_collision, rospy.Duration(2 * updatePeriod))
+  global use_collision
+  global lifetime
+  marker_msgs = link2marker_msg(link, full_linkinstancename, use_collision, lifetime)
   if len(marker_msgs) > 0:
     for marker_msg in marker_msgs:
       markerPub.publish(marker_msg)
@@ -50,33 +55,46 @@ def load_model(model_name):
   return model
 
 def on_model_states_msg(model_states_msg):
-  global lastUpdateTime
-  sinceLastUpdateDuration = rospy.get_rostime() - lastUpdateTime
-  if sinceLastUpdateDuration.to_sec() < updatePeriod:
-    return
-  lastUpdateTime = rospy.get_rostime()
+  global max_messages
+  global message_count
+  if max_messages <= 0 or message_count < max_messages:
+    global lastUpdateTime
+    sinceLastUpdateDuration = rospy.get_rostime() - lastUpdateTime
+    if sinceLastUpdateDuration.to_sec() < updatePeriod:
+      return
+    lastUpdateTime = rospy.get_rostime()
 
-
-  for (model_idx, modelinstance_name) in enumerate(model_states_msg.name):
-    try:
-      model = load_model(modelinstance_name)
-      assert(model)
-    except Exception:
+    for (model_idx, modelinstance_name) in enumerate(model_states_msg.name):
       try:
-        model_name = pysdf.name2modelname(modelinstance_name)
-        model = load_model(model_name)
+        model_name = modelinstance_name
+        if model_name in submodelsToBeIgnored:
+          continue
+        model = load_model(modelinstance_name)
         assert(model)
       except Exception:
-        rospy.logwarn('gazebo2marker_node: Failed to load model: {}'.format(modelinstance_name))
-        continue
-    rospy.logdebug('gazebo2marker_node: Successfully loaded model: {}'.format(modelinstance_name))
-    model.for_all_links(publish_link_marker, model_name=model_name, instance_name=modelinstance_name)
+        try:
+          model_name = pysdf.name2modelname(modelinstance_name)
+          if model_name in submodelsToBeIgnored:
+            continue
+          model = load_model(model_name)
+          assert(model)
+        except Exception:
+          rospy.logwarn('gazebo2marker_node: Failed to load model: {}'.format(modelinstance_name))
+          continue
+      rospy.logdebug('gazebo2marker_node: Successfully loaded model: {}'.format(modelinstance_name))
+      model.for_all_links(publish_link_marker, model_name=model_name, instance_name=modelinstance_name)
+    message_count += 1
+  else:
+    return
 
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-f', '--freq', type=float, default=2, help='Frequency Markers are published (default: 2 Hz)')
   parser.add_argument('-c', '--collision', action='store_true', help='Publish collision instead of visual elements')
+  parser.add_argument('-l', '--latch', action='store_true', help='Latch publisher.')
+  parser.add_argument('-lf', '--lifetime-forever', action='store_true', help='Set marker message duration lifetime to forever.')
+  parser.add_argument('-m', '--max-messages', type=int, default=-1, help='Maximum number of messages to publish per marker (default: -1 = infinite)')
   parser.add_argument('-w', '--worldfile', type=str, help='Read models from this world file')
   args = parser.parse_args(rospy.myargv()[1:])
 
@@ -92,6 +110,19 @@ def main():
   global use_collision
   use_collision = args.collision
 
+  global latch
+  if args.latch:
+    latch = True
+
+  global max_messages
+  max_messages = args.max_messages
+
+  global lifetime
+  if args.lifetime_forever:
+    lifetime = rospy.Duration(0)
+  else:
+    lifetime = rospy.Duration(2 * updatePeriod)
+
   if args.worldfile:
     global worldsdf
     global submodelsToBeIgnored
@@ -103,7 +134,7 @@ def main():
       rospy.logwarn('gazebo2marker_node: Failed to load world model {}'.format(args.worldfile))
 
   global markerPub
-  markerPub = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
+  markerPub = rospy.Publisher('/visualization_marker', Marker, queue_size=10, latch=latch)
   rospy.sleep(rospy.Duration(0, 100 * 1000))
 
   global lastUpdateTime
